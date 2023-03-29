94eb175ca03c0f17874a4e497742b3d7c5d54f32
DongHyeon Shin <101113835+OneMain1@users.noreply.github.com>
2023-03-29 23:13:15 +0900

    0.1 Input

---
+++ 0.1 Input.py
@@ -0,0 +1,20 @@
+
+# CTRL + K, C -> transform from code to comments
+# CTRL + K, U -> transform from comments to code 
+
+
+Num = int(input())
+print(Num * Num, end='\n')
+
+FloatNum = float(input())
+print(FloatNum * FloatNum, end='\n')
+
+Str = str(input())
+print(Str, end='\n')
+
+List = list(input())
+print(List, end='\n')
+
+
+
+# CTRL + R, CTRL + R -> Change the variable name with all refered variables
